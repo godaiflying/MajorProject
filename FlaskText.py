6d5f6a0b33ef7712e3f2ffd26f33c82576e400a9
godaiflying <31681293+godaiflying@users.noreply.github.com>
2022-05-01 14:13:59 +1000

    Attempting to code python in the html file

--- FlaskText.py
+++ FlaskText.py
@@ -1,9 +1,15 @@
-from flask import Flask 
+from flask import Flask, render_template, redirect, url_for 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return "<h1>Hello world!</h1>"
+    return render_template('index.html', content="cringe")
+
+
+#using redirect to make sure that only admins can access this url
+@app.route("/admin/")
+def admin():
+    return redirect(url_for("hello_world"))  
 
 def create_app():
     return app 
