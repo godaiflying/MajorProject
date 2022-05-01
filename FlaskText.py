ae348a3b009ad298408315eac65e593fa8c3ab5b
godaiflying <31681293+godaiflying@users.noreply.github.com>
2022-05-01 13:39:41 +1000

    Now works consistantly
    Using waitress-serve directly connnect it to a local host server.

--- FlaskText.py
+++ FlaskText.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return "Hello world"
+    return "<h1>Hello world!</h1>"
 
-if __name__ == "__main__":
-    app.run(debug=True)
+def create_app():
+    return app 
