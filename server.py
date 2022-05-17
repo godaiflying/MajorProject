907cea7b1dbd0bfd6ae7b7ef554791d5fdcf61a1
godaiflying <31681293+godaiflying@users.noreply.github.com>
2022-05-17 14:32:30 +1000

    listens
    :)

--- server.py
+++ server.py
@@ -7,7 +7,7 @@ HEADER_LENGTH = 10
 IP = "127.0.0.1"
 PORT = 1234
 
-server_socket = socket.socker(socket.AF_INET, socket.SOCK_STREAM)
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 #makes sure that the port can be reused 
 server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
@@ -16,3 +16,8 @@ server_socket.bind((IP, PORT))
 server_socket.listen()
 
 sockets_list = [server_socket]
+
+#list of users
+clients= {}
+
+print(f"Listening for connections on {IP}:{PORT}...")
