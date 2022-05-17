e9c8e38d368c85f619ff94df7c5bb25691a87e43
godaiflying <31681293+godaiflying@users.noreply.github.com>
2022-05-17 14:29:16 +1000

    setting up sockets
    setting up sockets so that they can use the same post.

---
+++ server.py
@@ -0,0 +1,18 @@
+import socket
+import select 
+#using select
+
+
+HEADER_LENGTH = 10
+IP = "127.0.0.1"
+PORT = 1234
+
+server_socket = socket.socker(socket.AF_INET, socket.SOCK_STREAM)
+
+#makes sure that the port can be reused 
+server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+server_socket.bind((IP, PORT))
+server_socket.listen()
+
+sockets_list = [server_socket]
