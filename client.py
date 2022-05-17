f40369315526b66c3ab3d6e84e450b929da485f9
godaiflying <31681293+godaiflying@users.noreply.github.com>
2022-05-17 19:48:32 +1000

    Starting

--- client.py
+++ client.py
@@ -7,7 +7,16 @@ HEADER_LENGTH = 10
 
 IP = "127.0.0.1"
 PORT = 1234
-username = input("Username: ")
+my_username = input("Username: ")
 
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client_socket.connect((IP,PORT))
+
+#recv block set to false
+client_socket.setblocking(False)
+
+username = my_username.encode('utf-8')
+username_header = f"{len(username):<{HEADER_LENGTH}}".encode('utf-8')
+
+while True:
+    message = input(f"{my_username} > ")
